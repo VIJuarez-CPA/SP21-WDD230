595a9d8fe967205e818059bf1dbb19d92787944c
VIJuarez-CPA <victorignjuarez@gmail.com>
2021-05-01 16:44:29 -0600

    xommit

---
+++ js/update.js
@@ -0,0 +1,6 @@
+// Update the copyright year
+document.queryselector('#year').textContent = new Date().getFullYear();
+// Provide the last Modified Date
+document.getElementById('update').textContent = document.lastModified;
+
+
